9284e5eee71c885b366687f08c9333e001a40410
Hesham Donia <hesham.khaled@code95.com>
2018-08-19 14:14:38 +0200

    Initial Commit

---
+++ SocialSignIn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialSignIn
+//
+//  Created by apple on 8/19/18.
+//  Copyright © 2018 Hesham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
